98b8c3a46acd99a89482ae9076d474d30f62fcc2
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2024-01-29 21:04:10 +0100

    Initial commit

---
+++ insert-into-a_user.sql
@@ -0,0 +1,12 @@
+INSERT INTO "a_user" (firstname, lastname, address, zip_code, city)
+VALUES
+    ('Jean', 'Dupont', '123 Rue de la Poste', '75001', 'Paris'),
+    ('Marie', 'Martin', '456 Avenue des Fleurs', '13001', 'Marseille'),
+    ('Pierre', 'Durand', '789 Boulevard du Commerce', '69001', 'Lyon'),
+    ('Sophie', 'Leclerc', '101 Rue de la Mairie', '31000', 'Toulouse'),
+    ('Luc', 'Lefebvre', '222 Chemin des Bois', '06000', 'Nice'),
+    ('Isabelle', 'Dupuis', '789 Avenue des Roses', '59000', 'Lille'),
+    ('Thomas', 'Robert', '555 Rue de la République', '33000', 'Bordeaux'),
+    ('Elodie', 'Girard', '999 Boulevard de la Liberté', '44000', 'Nantes'),
+    ('Antoine', 'Petit', '777 Rue de la Paix', '67000', 'Strasbourg'),
+    ('Julie', 'Dubois', '123 Avenue des Champs-Élysées', '83000', 'Toulon');
---
+++ select-inner-join-a_user-phone.sql
@@ -0,0 +1,2 @@
+select * from a_user
+inner join phone on a_user.id = phone.user_id

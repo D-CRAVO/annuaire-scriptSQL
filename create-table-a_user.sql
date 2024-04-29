e16a2a178d9a48afbeaebccff93bc70c90b4c291
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2024-04-29 13:52:56 +0200

    update

---
+++ create-table-a_user.sql
@@ -0,0 +1,8 @@
+CREATE TABLE a_user(
+id SERIAL PRIMARY KEY,
+firstname VARCHAR(50),
+lastname VARCHAR(50),
+address VARCHAR(255),
+zip_code CHAR(5),
+city VARCHAR(50)
+);
--- insert-into-a_user.sql
+++ insert-into-a_user.sql
@@ -1,12 +1,12 @@
-INSERT INTO "a_user" (firstname, lastname, address, zip_code, city)
+INSERT INTO "a_user" (id,firstname, lastname, address, zip_code, city)
 VALUES
-    ('Jean', 'Dupont', '123 Rue de la Poste', '75001', 'Paris'),
-    ('Marie', 'Martin', '456 Avenue des Fleurs', '13001', 'Marseille'),
-    ('Pierre', 'Durand', '789 Boulevard du Commerce', '69001', 'Lyon'),
-    ('Sophie', 'Leclerc', '101 Rue de la Mairie', '31000', 'Toulouse'),
-    ('Luc', 'Lefebvre', '222 Chemin des Bois', '06000', 'Nice'),
-    ('Isabelle', 'Dupuis', '789 Avenue des Roses', '59000', 'Lille'),
-    ('Thomas', 'Robert', '555 Rue de la République', '33000', 'Bordeaux'),
-    ('Elodie', 'Girard', '999 Boulevard de la Liberté', '44000', 'Nantes'),
-    ('Antoine', 'Petit', '777 Rue de la Paix', '67000', 'Strasbourg'),
-    ('Julie', 'Dubois', '123 Avenue des Champs-Élysées', '83000', 'Toulon');
+    (1,'Jean', 'Dupont', '123 Rue de la Poste', '75001', 'Paris'),
+    (2,'Marie', 'Martin', '456 Avenue des Fleurs', '13001', 'Marseille'),
+    (3,'Pierre', 'Durand', '789 Boulevard du Commerce', '69001', 'Lyon'),
+    (4,'Sophie', 'Leclerc', '101 Rue de la Mairie', '31000', 'Toulouse'),
+    (5,'Luc', 'Lefebvre', '222 Chemin des Bois', '06000', 'Nice'),
+    (6,'Isabelle', 'Dupuis', '789 Avenue des Roses', '59000', 'Lille'),
+    (7,'Thomas', 'Robert', '555 Rue de la République', '33000', 'Bordeaux'),
+    (8,'Elodie', 'Girard', '999 Boulevard de la Liberté', '44000', 'Nantes'),
+    (9,'Antoine', 'Petit', '777 Rue de la Paix', '67000', 'Strasbourg'),
+    (10,'Julie', 'Dubois', '123 Avenue des Champs-Élysées', '83000', 'Toulon');
